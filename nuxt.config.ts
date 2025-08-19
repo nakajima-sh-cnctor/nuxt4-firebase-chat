bccfe4aebb1d05109bbccee7dbe2bbfd5009f9c3
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-08-19 14:51:13 +0900

    vuetify-nuxt-moduleを追加し、Firebase認証画面を実装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'vuetify-nuxt-module'],
 
   // Firebase設定
   runtimeConfig: {
