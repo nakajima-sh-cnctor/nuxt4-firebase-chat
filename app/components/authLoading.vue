8559a7d946a8dfaa16df0b44e69b3c3234f9c75c
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-09-10 14:55:53 +0900

    認証ローディングコンポーネントを新規作成し、ホームに統合

---
+++ app/components/authLoading.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { loading } = useAuth()
+</script>
+
+<template>
+  <div>
+    <!-- ローディング状態 -->
+    <div v-if="loading" class="loading">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        size="64"
+      ></v-progress-circular>
+      <p class="mt-4">認証状態を確認中...</p>
+    </div>
+    <div v-else>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loading {
+  text-align: center;
+  padding: 4rem 0;
+}
+</style>
