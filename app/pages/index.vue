f5f13e9dde1c0bfefb7eef4546f18dc342fbb688
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-10-17 10:25:44 +0900

    indexは今後認証なしで見れるページにしたいので暫定処置の修正

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,27 +1,11 @@
 <script setup lang="ts">
-import AuthLoading from '~/components/AuthLoading.vue'
-import { useAuthStore } from '~/stores/auth'
-
-const { startAuthListener, stopAuthListener } = useAuthStore()
-const { loading } = toRefs(useAuthStore())
-
 onMounted(async () => {
-  startAuthListener()
-
-  // 認証状態の確認とリダイレクト
-  await nextTick()
-
-  if (!loading.value) {
-    // ログイン済みの場合はhomeページにリダイレクト
-    await navigateTo('/home')
-  }
-})
-
-onUnmounted(() => {
-  stopAuthListener()
+  await navigateTo('/home')
 })
 </script>
 
 <template>
-  <AuthLoading :loading="loading" />
+  <div>
+    <h1>Hello World</h1>
+  </div>
 </template>
